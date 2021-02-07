a7d361682fd10d7f02432cce1bacf3fd44cc9c8b
Ravindra <rp@shoutrlabs.com>
2021-02-07 23:00:18 +0100

    initial check-in

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+
+const path = __dirname + '/views/';
+const port = 8080;
+
+router.use(function(req, res, next) {
+    console.log(req.method);
+    next();
+});
+router.get('/', function(req, res) {
+    res.send(path + 'index.html');
+});
+app.use(express.static(path));
+app.use('/', router);
+
+app.listen(port, function() {
+    console.log('App listening on port', port);
+});
